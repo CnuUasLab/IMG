d5e38631b17753dbf3000c1eb9b345182d4a0708
davis.catherman.14 <daviscatherman@gmail.com>
2017-02-09 17:26:22 -0500

    rectangles are successfully drawn

--- img.py
+++ img.py
@@ -4,40 +4,41 @@ import Tkinter as tk
 import Image, ImageTk
 import sys
 
+pts = []
+pt0 = 0,0
+pt1 = 0,0
+
+image = np.zeros((1,1,3), np.uint8)
 #Set up GUI
 window = tk.Tk()  #Makes main window
 window.wm_title("Image Manipulation Genie (IMG)")
 window.config(background="#FFFFFF")
 
+#window.geometry("%dx%d+%d+%d" % (1500, 750, 1, 1))
+
 #Graphics window
-imageFrame = tk.Frame(window, width=600, height=500)
+imageFrame = tk.Frame(window, width=1500, height=750)
 imageFrame.grid(row=0, column=0, padx=10, pady=2)
 
 lmain = tk.Label(imageFrame)
 lmain.grid(row=0, column=0)
 
-image = cv2.imread("sample.jpg")
-image = cv2.resize(image, (1000, 500))
-clone = image.copy()
-
-# for keyboard cmds or for quiting
-def update_key_press(event):
-    global image, clone
-    # if the 'r' key is pressed, reset the cropping region
-    if event.keysym == 'r':
-        image = clone.copy()
- 
-    # if the 'q' key is pressed, break from the loop
-    elif event.keysym == 'q':
-        sys.exit(0)
-
-pt0 = 0,0
-pt1 = 0,0
+def crop_roi():
+    print "cropping current ROIs"
+    
 
 # ensures positive area and draws the rectangle
 def make_rectangle():
+
     global image, pt0, pt1
-    cv2.rectangle(image, pt0, pt1, (0, 255, 0), 2)
+
+    # ensures the set of two pts is in order from lowest to highest
+    # so that "high-low >= 0" is true
+    pts.append(min(pt0,pt1))
+    pts.append(max(pt0,pt1))
+
+    # draws rectangle at two pts in color red (BGR) with width 2
+    cv2.rectangle(image, pt0, pt1, (0, 0, 255), 2)
 
 # for click-n-crop feature
 def mouse_press(event):
@@ -47,9 +48,29 @@ def mouse_press(event):
 def mouse_release(event):
     global pt1
     pt1 = event.x, event.y
-
     make_rectangle()
 
+# for keyboard cmds or for quiting
+def update_key_press(event):
+
+    global image, clone, pts
+
+    # if the 'r' key is pressed, reset the cropping region
+    if event.keysym == 'r':
+        image = clone.copy()
+        pts = []
+        print "Regions of interests cleared"
+ 
+    # if the 'p' key is pressed, print the pt list
+    if event.keysym == 'p':
+        print "Printing points..."
+        for i in range(0,len(pts),2):
+            print pts[i], pts[i+1]
+
+    # if the 'q' key is pressed, break from the loop
+    elif event.keysym == 'q':
+        print "Exiting on q-press"
+        sys.exit(0)
 
 def show_image():
 
@@ -57,21 +78,42 @@ def show_image():
 
     cv2image = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
 
-
     img = Image.fromarray(cv2image)
 
     imgtk = ImageTk.PhotoImage(image=img)
     lmain.imgtk = imgtk
 
     lmain.configure(image=imgtk)
-    lmain.after(10, show_image)
 
-#Slider window (slider controls stage position)
-sliderFrame = tk.Frame(window, width=500, height=100)
-sliderFrame.grid(row = 600, column=0, padx=10, pady=2)
+def image_loop():
 
-show_image()  #Display 2
-window.bind_all('<KeyPress>', update_key_press)
-window.bind_all('<ButtonPress-1>', mouse_press)
-window.bind_all('<ButtonRelease-1>', mouse_release)
-window.mainloop() #Starts GUI
+    show_image()
+    lmain.after(10, image_loop)
+
+def main():
+
+    global image
+
+    image = cv2.imread("sample.jpg")
+    image = cv2.resize(image, (1500, 750))
+    clone = image.copy()
+
+    #Slider window (slider controls stage position)
+    sliderFrame = tk.Frame(window, width=500, height=100)
+    sliderFrame.grid(row = 600, column=0, padx=10, pady=2)
+
+    image_loop() # display image function
+
+    # bind the following three to the main loop
+    window.bind_all('<KeyPress>', update_key_press)
+    window.bind_all('<ButtonPress-1>', mouse_press)
+    window.bind_all('<ButtonRelease-1>', mouse_release)
+
+
+    # run the main loop of tkinter
+    window.mainloop() #Starts GUI
+
+
+
+if __name__ == "__main__":
+    main()
