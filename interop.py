718a6a38872f4d817e89af3a828a9b0f1869dc9a
Catherman <dcatherm@ndc.nasa.gov>
2017-05-30 14:42:05 -0400

    cleanup of basic shell methods

--- interop.py
+++ interop.py
@@ -5,11 +5,25 @@ class interop:
 		self.targetsSubmitted = []
 		self.numSubmissions = 0
 		self.numTargetsSubmitted = 0
-		self.resonse = ""
 
-	def submit_targets(self):
+	def submit_all_targets(self):
+		self.check_for_edits()
 		self.check_for_duplicates()
 		
+		for targ in targetsPreSubmit:
+			self.submit_single_target(targ)
+
+	def submit_single_target(self, targ):
+		print "submitting single target"
+
+	def submit_edits(self, targ):
+		print "special submission rules for submitting targ"
+
+	def check_for_edits(self):
+		print "if target is edit, special condition necessary"
+		for targ in targetsPreSubmit:
+			if True:
+				submit_edits(self, targ)
 
 	def check_for_duplicates(self):
 		print "dummy duplicate check"
