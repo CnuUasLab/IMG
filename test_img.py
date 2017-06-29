683f6300f75df9dd227460cc40ffbd6438f0fb10
David Kroell <dkroell@acm.org>
2017-06-28 22:11:16 -0400

    updating pytest

--- test_img.py
+++ test_img.py
@@ -1,21 +1,20 @@
 #=======================================#
 #										#
 #	  Test file used to test this		#
-#	      python build for IMG.			#
+#	    python build for img.py			#
 #										#
-# 		   Author: davidkroell		  	#
+# 		  Author: davidkroell		  	#
 #										#
 #=======================================#
 
-import numpy as np
-import cv2
-import Tkinter as tk
-from PIL import Image, ImageTk
-import glob
-import sys
-from enum import Enum
-from button_press import key_press, mouse_press, mouse_release
-from image_object import img_obj as img_obj
-from var import mode
+import py, pytest
+from img import IMG
 
-#===== TESTING DEPENDENCIES ONLY RIGHT NOW =====#
+def func_instantiation():
+	try:
+    	imgage_obj = IMG()
+    except Error:
+    	pytest.fail("Unexpected Error in instantiation.")
+
+def test_instantiation():
+    assert func_instantiation() == 0
