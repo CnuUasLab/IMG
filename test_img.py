43b2cf9e7a00213298e6769f34a4ffeb18d18f43
David Kroell <dkroell@acm.org>
2017-06-28 22:04:23 -0400

    Adding img test sample

---
+++ test_img.py
@@ -0,0 +1,21 @@
+#=======================================#
+#										#
+#	  Test file used to test this		#
+#	      python build for IMG.			#
+#										#
+# 		   Author: davidkroell		  	#
+#										#
+#=======================================#
+
+import numpy as np
+import cv2
+import Tkinter as tk
+from PIL import Image, ImageTk
+import glob
+import sys
+from enum import Enum
+from button_press import key_press, mouse_press, mouse_release
+from image_object import img_obj as img_obj
+from var import mode
+
+#===== TESTING DEPENDENCIES ONLY RIGHT NOW =====#
